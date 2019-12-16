c416a3dc9663d750fb5bc9ae1ddf5974cbeb5de4
Jose Segura <jose.segura@owlstone.co.uk>
2019-12-16 09:34:07 +0000

    Added some more git aliases

--- .git-completion.bash
+++ .git-completion.bash
@@ -3,10 +3,14 @@ source /usr/share/bash-completion/completions/git
 
 # Git completions for git aliases
 __git_complete s _git_status
-__git_complete c _git_commit
+__git_complete cm _git_commit
 __git_complete ck _git_checkout
 __git_complete b _git_branch
 __git_complete l _git_log
 __git_complete r _git_reset
 __git_complete a _git_add
 __git_complete d _git_diff
+__git_complete dv _git_diff
+__git_complete rb _git_rebase
+__git_complete pf _git_push
+__git_complete pullr _git_pull
